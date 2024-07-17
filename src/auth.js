67792c01ab25d3c287f3a00c70bc9e236cfd1739
DeepakChowdry-bit <deepakchowdry07@gmail.com>
2024-07-17 16:57:47 +0530

    Transformed totally

---
+++ src/auth.js
@@ -0,0 +1,25 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    })
+  ],
+  callbacks: {
+    async session({ session }) {
+      return session;
+    },
+    async signIn({ profile }) {
+      console.log(profile);
+      return true
+    },
+  },
+});
---
+++ src/components/Signin.jsx
@@ -0,0 +1,80 @@
+import { auth, signIn } from "@/auth";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { redirect } from "next/navigation";
+import { FcGoogle } from "react-icons/fc";
+import { IoLogoApple, IoLogoGithub } from "react-icons/io5";
+import { DropdownMenuSeparator } from "./ui/dropdown-menu";
+
+export async function SignIn() {
+  const session = await auth();
+
+  if (session?.user) {
+    redirect("/");
+  }
+  return (
+    <div className="flex items-center justify-center min-h-screen w-full">
+      <Card className="w-[90%] md:w-[400px]">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>login to your account in one-click.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="max-w-full flex flex-col items-center gap-2">
+            <form
+              action={async () => {
+                "use server";
+                await signIn("google");
+              }}
+              className="w-full"
+            >
+              <Button
+                variant="outline"
+                className="w-full flex items-center gap-2"
+              >
+                <FcGoogle className="text-xl" /> Google
+              </Button>
+            </form>
+            <form
+              action={async () => {
+                "use server";
+                await signIn("google");
+              }}
+              className="w-full"
+            >
+              <Button
+                variant="outline"
+                className="w-full flex items-center gap-2"
+              >
+                <IoLogoApple className="text-xl" /> Apple
+              </Button>
+            </form>
+
+            <DropdownMenuSeparator />
+
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+              className="w-full"
+            >
+              <Button
+                variant="default"
+                className="w-full flex items-center gap-2"
+              >
+                <IoLogoGithub className="text-xl" /> Github
+              </Button>
+            </form>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
